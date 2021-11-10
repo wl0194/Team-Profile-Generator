03491c6e58d63e1f6374457de984dc2201bcf8b1
Amy Hwang <728amyh@gmail.com>
2021-11-09 20:42:43 -0500

    test passed

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -3,11 +3,11 @@ const Engineer = require('../lib/Engineer.js');
 test ("Can set Github account via constructor", () => {
     const testval = 'GithubUser';
     const engineer = new Engineer('Bob', 1, "test@test.com", testval);
-    expect(engineer.github()).toBe(testval);
+    expect(engineer.getGithub()).toBe(testval);
 });
 test ("getRole() should return \"Engineer\"", () => {
     const testval = "Engineer"
-    const engineer = new Engineer('Sarah', 1, 'test@test.com');
+    const engineer = new Engineer('Bob', 1, 'test@test.com');
     expect(engineer.getRole()).toBe(testval);
 });
 test("Can we get GitHub username via getGithub()", () => {
