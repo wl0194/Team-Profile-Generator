65fcea909dee9246c537bf26a80acc5e7fb1bb92
Amy Hwang <728amyh@gmail.com>
2021-11-09 23:53:59 -0500

    add array of questions

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Team-Profile-Generator
+## Title
+
+Team Profile Generator
+
+## Description
+
+This is a Node.js command-line application that takes in information about employees on a software engineering team, then generates an HTML webpage that displays summaries for each person. Testing is key to making code maintainable, so I’ll also write a unit test for every part of my code and ensure that it passes each test.
+
+## Demo
+
+
+## Links
+
+## License
+
+MIT
--- index.js
+++ index.js
@@ -4,33 +4,108 @@ const Intern = require('./lib/Intern.js');
 const Manager = require('./lib/Manager.js');
 const inquirer = require('inquirer');
 const fs = requre('fs');
+const path = require('path');
 
 const questions = {
-    Employee = [
+    Manager = [
         {
             type:"input",
             name:"name",
-            message:"What is the name of the Employee?"
+            message:"What is the manager's name?"
 
         },
         {
             type:"input",
             name:"id",
-            message:"What is the id of the Employee?"
+            message:"What is the manager's ID?"
 
         },
         {
             type:"input",
             name:"email",
-            message:"What is the email of the Employee?"
+            message:"What is the manager's email?"
 
         },
-    
+        {
+            type:"input",
+            name:"officeNumber",
+            message:"What is the manager's office number?"
 
+        },
+        {
+            type:"list",
+            name:"addNew",
+            message:"Do you want to add another employee?",
+            choices:["yes", "no"]
 
+        },
+    ], 
+
+    Engineer = [
+        {
+            type:"input",
+            name:"name",
+            message:"What is the engineer's name?"
+
+        },
+        {
+            type:"input",
+            name:"id",
+            message:"What is the engineer's ID?"
+
+        },
+        {
+            type:"input",
+            name:"email",
+            message:"What is the engineer's email?"
+
+        },
+        {
+            type:"input",
+            name:"github",
+            message:"What is the engineer's GitHub account?"
+
+        },
+        {
+            type:"list",
+            name:"addNew",
+            message:"Do you want to add another employee?",
+            choices:["yes", "no"]
+
+        },
+    ],
+    Intern = [
+        {
+            type:"input",
+            name:"name",
+            message:"What is the Intern's name?"
+
+        },
+        {
+            type:"input",
+            name:"id",
+            message:"What is the Intern's ID?"
+
+        },
+        {
+            type:"input",
+            name:"email",
+            message:"What is Intern's email?"
+
+        },
+        {
+            type:"input",
+            name:"school",
+            message:"What school is the intern attending?"
+
+        },
+        {
+            type:"list",
+            name:"addNew",
+            message:"Do you want to add another employee?",
+            choices:["yes", "no"]
+
+        },
     ]
+};
 
-
-
-
-}
