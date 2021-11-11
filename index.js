2513bbd443dc0a5a09a879e4104496d45086c9aa
Amy Hwang <728amyh@gmail.com>
2021-11-11 11:15:04 -0500

    change array setting

--- index.js
+++ index.js
@@ -4,183 +4,183 @@ const Manager = require('./lib/Manager.js');
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
-// const generateHTML = require('./generateHTML.js');
+const generateHTML = require('./generateHTML.js');
 
 // an array of objects 
-const questions = ([
-    Manager = 
-        {
-            type:"input",
-            name:"name",
-            message:"What is the manager's name?",
-            validate: (value) => {
-                if(value) {
-                    return true
-                } else { return "Please enter manager's name."}
-            },
+const managerQuestions = ([
+    {
+        type: "input",
+        name: "name",
+        message: "What is the manager's name?",
+        validate: (value) => {
+            if (value) {
+                return true
+            } else { return "Please enter manager's name." }
+        },
 
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the manager's ID?",
+        validate: (value) => {
+            if (value) {
+                return true
+            } else { return "Please enter manager's ID." }
         },
-        {
-            type:"input",
-            name:"id",
-            message:"What is the manager's ID?",
-            validate: (value) => {
-                if(value) {
-                    return true
-                } else { return "Please enter manager's ID."}
-            },
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the manager's email?",
+        validate: (value) => {
+            if (value) {
+                return true
+            } else { return "Please enter manager's email." }
         },
-        {
-            type:"input",
-            name:"email",
-            message:"What is the manager's email?",
-            validate: (value) => {
-                if(value) {
-                    return true
-                } else { return "Please enter manager's email."}
-            },
 
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the manager's office number?",
+        validate: (value) => {
+            if (value) {
+                return true
+            } else { return "Please enter manager's office number." }
         },
-        {
-            type:"input",
-            name:"officeNumber",
-            message:"What is the manager's office number?",
-            validate: (value) => {
-                if(value) {
-                    return true
-                } else { return "Please enter manager's office number."}
-            },
 
-        },
-        {
-            type:"list",
-            name:"addNew",
-            message:"Do you want to add another employee?",
-            choices:["yes", "no"]
+    },
+    {
+        type: "list",
+        name: "addNew",
+        message: "Do you want to add another employee?",
+        choices: ["yes", "no"]
 
-        },
-    
-
-    Engineer = 
-        {
-            type:"input",
-            name:"name",
-            message:"What is the engineer's name?",
-            validate: (value) => {
-                if(value) {
-                    return true
-                } else { return "Please enter engineer's name."}
-            },
-
-        },
-        {
-            type:"input",
-            name:"id",
-            message:"What is the engineer's ID?",
-            validate: (value) => {
-                if(value) {
-                    return true
-                } else { return "Please enter engineer's ID."}
-            },
-
-        },
-        {
-            type:"input",
-            name:"email",
-            message:"What is the engineer's email?",
-            validate: (value) => {
-                if(value) {
-                    return true
-                } else { return "Please enter engineer's email."}
-            },
-
-        },
-        {
-            type:"input",
-            name:"github",
-            message:"What is the engineer's GitHub account?",
-            validate: (value) => {
-                if(value) {
-                    return true
-                } else { return "Please enter engineer's Github account."}
-            },
-
-        },
-        {
-            type:"list",
-            name:"addNew",
-            message:"Do you want to add another employee?",
-            choices:["yes", "no"]
-
-        },
-    ,
-
-    Intern = 
-        {
-            type:"input",
-            name:"name",
-            message:"What is the Intern's name?",
-            validate: (value) => {
-                if(value) {
-                    return true
-                } else { return "Please enter Intern's name."}
-            },
-
-        },
-        {
-            type:"input",
-            name:"id",
-            message:"What is the Intern's ID?",
-            validate: (value) => {
-                if(value) {
-                    return true
-                } else { return "Please enter Intern's ID."}
-            },
-        },
-        {
-            type:"input",
-            name:"email",
-            message:"What is Intern's email?",
-            validate: (value) => {
-                if(value) {
-                    return true
-                } else { return "Please enter Intern's email."}
-            },
-        },
-        {
-            type:"input",
-            name:"school",
-            message:"What school is the intern attending?",
-            validate: (value) => {
-                if(value) {
-                    return true
-                } else { return "Please enter Intern's school name"}
-            },
-        },
-        {
-            type:"list",
-            name:"addNew",
-            message:"Do you want to add another employee?",
-            choices:["yes", "no"]
-        },
+    },
 ]);
 
+const engineerQuestions = ([
+    {
+        type: "input",
+        name: "name",
+        message: "What is the engineer's name?",
+        validate: (value) => {
+            if (value) {
+                return true
+            } else { return "Please enter engineer's name." }
+        },
+
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the engineer's ID?",
+        validate: (value) => {
+            if (value) {
+                return true
+            } else { return "Please enter engineer's ID." }
+        },
+
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the engineer's email?",
+        validate: (value) => {
+            if (value) {
+                return true
+            } else { return "Please enter engineer's email." }
+        },
+
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is the engineer's GitHub account?",
+        validate: (value) => {
+            if (value) {
+                return true
+            } else { return "Please enter engineer's Github account." }
+        },
+
+    },
+    {
+        type: "list",
+        name: "addNew",
+        message: "Do you want to add another employee?",
+        choices: ["yes", "no"]
+
+    },
+]);
+
+const internQuestions = ([
+    {
+        type: "input",
+        name: "name",
+        message: "What is the Intern's name?",
+        validate: (value) => {
+            if (value) {
+                return true
+            } else { return "Please enter Intern's name." }
+        },
+
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the Intern's ID?",
+        validate: (value) => {
+            if (value) {
+                return true
+            } else { return "Please enter Intern's ID." }
+        },
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is Intern's email?",
+        validate: (value) => {
+            if (value) {
+                return true
+            } else { return "Please enter Intern's email." }
+        },
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What school is the intern attending?",
+        validate: (value) => {
+            if (value) {
+                return true
+            } else { return "Please enter Intern's school name" }
+        },
+    },
+    {
+        type: "list",
+        name: "addNew",
+        message: "Do you want to add another employee?",
+        choices: ["yes", "no"]
+
+    },
+]);
+
+
 function writeToFile(fileName, data) {
-    return  fs.writeFile(path.join(__dirname, fileName), data, function(err) {
-  if(err)
-  console.error(err);
+    return fs.writeFile(path.join(__dirname, fileName), data, function (err) {
+        if (err)
+            console.error(err);
     })
-  };
-  
-  // Create a function to initialize app
-  function init() {
-      inquirer.prompt(questions).then((inquirerResponses) => {
-          console.log('generating...');
-          writeToFile('main.html', generateHTML({ ...inquirerResponses }));
-      });
-  }
-  
-  
-  // Function call to initialize app
-  init();
-  
+};
+
+// Create a function to initialize app
+function init() {
+    inquirer.prompt(managerQuestions,engineerQuestions,internQuestions).then((inquirerResponses) => {
+        console.log('generating...');
+        writeToFile('main.html', generateHTML({ ...inquirerResponses }));
+    });
+}
+
+
+// Function call to initialize app
+init();
